429cae76f0b271884d75b68db5d3bb2b502dd3c0
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-10 21:43:32 +0900

    Update PageLayout.js

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -117,7 +117,7 @@ export default function ({children, isLoading}) {
                 <span className="flex-grow">
                      <Link href="/">
                     <a className="text-xl font-semibold focus:text-gray-500 hover:text-gray-700 focus:outline-none">
-                        Project Hunt
+                        Project <font color="#4285F4">Hunt</font>
                     </a>
                 </Link>
                 </span>
