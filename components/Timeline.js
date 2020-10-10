52124357be592297be9337762456a1b0bc9d5214
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-10 15:39:37 +0900

    Update Timeline.js

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,14 +1,41 @@
-import React from "react";
-import "./styles.css";
-import { Chrono } from "react-chrono";
-import data from "./data";
+import React from 'react';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
-export default function Timeline() {
+export default function OutlinedTimeline() {
   return (
-    <div className="Timeline">
-      <div style={{ width: "100%", height: "95vh" }}>
-        <Chrono items={data} mode="TREE" cardHeight={300} />
-      </div>
-    </div>
+    <Timeline align="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="secondary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" />
+        </TimelineSeparator>
+        <TimelineContent>Repeat</TimelineContent>
+      </TimelineItem>
+    </Timeline>
   );
 }
