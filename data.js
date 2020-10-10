860d760c3bbb2dc6a57e0b1c9a0afdd1ced50ea6
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-10 15:12:45 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const items = [
+  {
+    title: "0818",
+    contentTitle: "First Team meeting",
+    contentText: "Brown CIT",
+    contentDetailedText: `dkdk`
+  },
+
+  {
+    title: "0912",
+    contentTitle: "Second Team meeting",
+    contentText: "Three sisters Providence",
+    contentDetailedText: `dkdk`
+  }
+];
+
+export default items;
