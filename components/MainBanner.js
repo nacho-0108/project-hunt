22d7a4f1f978ec56c2304074535604a19689d600
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-11 22:38:15 +0900

    Update MainBanner.js

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -1,5 +1,15 @@
 function MyImage() {
   return <img src="/Banner_M.jpg" alt="my image" />
+      <style jsx>{`
+        .image-container: {
+          position: relative:
+        }
+        img {
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+    `}</style>
 }
 
 export default MyImage
